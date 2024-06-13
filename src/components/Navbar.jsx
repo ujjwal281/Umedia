be0f7bb7e3a1f230dfc663890b530b3f32819254
ujjwal281 <ujjwalnegi.1980@gmail.com>
2024-06-13 20:48:48 +0530

    title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+      <div style={{color:"white" , display : "flex" , justifyContent:"center", fontSize:"30px" , marginLeft:"5px" ,fontFamily:"sans-serif" }}>Umedia</div>
     </Link>
     <SearchBar />
   </Stack>
